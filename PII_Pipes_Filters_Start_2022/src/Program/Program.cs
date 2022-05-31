ba7551ff442a577924f7841cf463539395c25f53
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-05-31 20:22:41 -0300

    publicacion en twitter

--- PII_Pipes_Filters_Start_2022/src/Program/Program.cs
+++ PII_Pipes_Filters_Start_2022/src/Program/Program.cs
@@ -12,17 +12,17 @@ namespace CompAndDel
             IPicture NoFilterPicture = provider.GetPicture(@"luke.jpg");
             
             PipeNull pipeNull = new PipeNull();
+            
+            FilterGreyscale greyScale = new FilterGreyscale();
+            PipeSerial pipeSerialGrey = new PipeSerial(greyScale, pipeNull);
 
             FilterNegative filterNegative = new FilterNegative();
             PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipeNull);
-            
-            FilterGreyscale greyscale = new FilterGreyscale();
-            PipeSerial pipeSerialGrey = new PipeSerial(greyscale, pipeNull);
 
             provider.SavePicture(pipeNull.Send(NoFilterPicture), @"LukeNull.jpg");
             provider.SavePicture(pipeSerialNegative.Send(NoFilterPicture), @"LukeNegativeFilter.jpg");
             provider.SavePicture(pipeSerialGrey.Send(NoFilterPicture), @"LukeGreyFilter.jpg");
-
+            
         }
     }
 }
--- PII_TwitterApi-master/src/Program/Program.cs
+++ PII_TwitterApi-master/src/Program/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Threading.Tasks;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
+using CompAndDel;
+
 
 namespace TwitterUCU
 {
@@ -7,10 +11,23 @@ namespace TwitterUCU
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture NoFilterPicture = provider.GetPicture(@"bill2.jpg");
+            
+            PipeNull pipeNull = new PipeNull();
+            FilterGreyscale greyScale = new FilterGreyscale();
+            PipeSerial pipeSerialGrey = new PipeSerial(greyScale, pipeNull);
+            FilterNegative filterNegative = new FilterNegative();
+            PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipeNull);
+
+            provider.SavePicture(pipeNull.Send(NoFilterPicture), @"Bill2Null.jpg");
+            provider.SavePicture(pipeSerialNegative.Send(NoFilterPicture), @"Bill2NegativeFilter.jpg");
+            provider.SavePicture(pipeSerialGrey.Send(NoFilterPicture), @"Bill2GreyFilter.jpg");
+
             var twitter = new TwitterImage();
-            Console.WriteLine(twitter.PublishToTwitter("New Employee 2! ",@"bill2.jpg"));
-            var twitterDirectMessage = new TwitterMessage();
-            Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "122237423"));
+            Console.WriteLine(twitter.PublishToTwitter("New Employee 2! ",@"Bill2GreyFilter.jpg"));
+            // var twitterDirectMessage = new TwitterMessage();
+            // Console.WriteLine(twitterDirectMessage.SendMessage("Hola!", "249011461"));
         }
     }
 }

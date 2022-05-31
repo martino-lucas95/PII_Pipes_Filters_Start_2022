fbcd1898fe0c70e2dfefff22d2f71abc9ac3dcbf
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-05-31 19:37:14 -0300

    apply filters to Luke picture

--- PII_Pipes_Filters_Start_2022/src/Program/Program.cs
+++ PII_Pipes_Filters_Start_2022/src/Program/Program.cs
@@ -8,6 +8,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider provider = new PictureProvider();
+            IPicture NoFilterPicture = provider.GetPicture(@"luke.jpg");
+            
+            PipeNull pipeNull = new PipeNull();
+
+            FilterNegative filterNegative = new FilterNegative();
+            PipeSerial pipeSerialNegative = new PipeSerial(filterNegative, pipeNull);
+            
+            FilterGreyscale greyscale = new FilterGreyscale();
+            PipeSerial pipeSerialGrey = new PipeSerial(greyscale, pipeNull);
+
+            provider.SavePicture(pipeNull.Send(NoFilterPicture), @"LukeNull.jpg");
+            provider.SavePicture(pipeSerialNegative.Send(NoFilterPicture), @"LukeNegativeFilter.jpg");
+            provider.SavePicture(pipeSerialGrey.Send(NoFilterPicture), @"LukeGreyFilter.jpg");
+
         }
     }
 }
